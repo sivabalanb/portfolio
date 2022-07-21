8794c9af794c400d91377fea9d67bfcfe4eb29f5
sivabalan balasubramanian <sivabalanb.92@gmail.com>
2022-07-21 10:16:33 +0100

    New animation using framer motion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import AppBar from './AppBar/AppBar'
-
+import React from "react";
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Pages
+import Home from "./pages/home";
+import Model from "./pages/model";
+//components
+import Header from "./components/Header";
+//Styles
+import "./App.scss";
 
 function App() {
+  const imageDetails = {
+    width: 524,
+    height: 650,
+  };
+
   return (
-    <AppBar/>
+    <Router>
+      <Header />
+      <Route
+        render={({ location }) => (
+          <AnimatePresence initial={false} exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route
+                exact
+                path='/'
+                render={() => <Home imageDetails={imageDetails} />}
+              />
+              <Route
+                exact
+                path='/model/:id'
+                render={() => <Model imageDetails={imageDetails} />}
+              />
+            </Switch>
+          </AnimatePresence>
+        )}
+      />
+    </Router>
   );
 }
 
